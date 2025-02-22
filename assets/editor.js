b02e5c69c0c7c05c7091b21bd4e07bc949f58d54
joao <joaovitoroliveira4414@gmail.com>
2025-02-22 16:24:09 -0300

    Update editor.js

--- assets/editor.js
+++ assets/editor.js
@@ -12,7 +12,7 @@ let player = null;
 let selectedObject = null;  // Objeto que está sendo arrastado
 let offsetX, offsetY;
 const euaBallImg = new Image();
-euaBallImg.src = "euaball.png";
+euaBallImg.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKR6-9KcS5GYzpYRyJPFqRmvp2escLMnql4A&s";
 
 // Carrega um mapa salvo, se existir
 if (localStorage.getItem("mapaCustomizado")) {
@@ -142,7 +142,7 @@ canvas.addEventListener("mouseup", () => {
     selectedObject = null;
 });
 
-let euaBallImgSrc = "euaball.png";
+let euaBallImgSrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKR6-9KcS5GYzpYRyJPFqRmvp2escLMnql4A&s";
 
 // Verifica se o jogador já salvou uma imagem antes
 if (localStorage.getItem("euaBallImg")) {
